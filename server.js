6d3275a43cfa95e02ade8cdea60123faa10e2853
tomitorino <tomastorino98@gmail.com>
2022-03-08 11:59:27 -0300

    Initial commit

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const devices = require("./devices.json");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+mongoose.connect(
+  "mongodb+srv://admin-tomas:admin21123admin@cluster0.elc3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
 app.get("/", function (req, res) {
-  /*
   console.log(devices.RECORDS.length);
   const deviceOne = devices.RECORDS[1].name;
   const theRandom = Math.floor(Math.random() * (10633 - 1)) + 1;
@@ -21,8 +28,7 @@ app.get("/", function (req, res) {
   res.write("<img src='" + randomImg + "'></img>");
   //res.write("<h2>Name of this device is: " + deviceOne + "</h2>");
   res.send();
-  */
-  res.sendFile(__dirname + "/index.html");
+  //res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/index.html", function (req, res) {
